c7cf7d90d463fadd07b416336bf44eda2fdf237e
Isaac <isogunro@gmail.com>
2020-01-14 21:07:07 -0500

    updates

--- siteLookup.js
+++ siteLookup.js
@@ -1,60 +1,60 @@
 new Vue({
-	el:"#app",
+	el: "#app",
 	data: {
 		test: "Hello World",
 		pg: [],
 		searchTbl: '',
 		regionDD: '',
-		url: 'https://isogunro.sharepoint.com/sites/demos/mdemos/Lists/Site%20Lookup/EditForm.aspx?ID=',
-		urlEnd: '&Source=https://isogunro.sharepoint.com/sites/demos/mdemos/SitePages/siteLookup.aspx'
+		url: '../sites/demos/mdemos/Lists/Site%20Lookup/EditForm.aspx?ID=',
+		urlEnd: '&Source=../sites/demos/mdemos/SitePages/siteLookup.aspx'
 	},
-	created: function() {
+	created: function () {
 		this.getListData('Site Lookup');
 	},
 	methods: {
-		getListData: function(lname){
-		    var endPointUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('"+lname+"')/items";
-	        var headers = {
-	        		"accept": "application/json;odata=verbose"
-	            };
-	        
-	        var that = this;   
-					axios.get(endPointUrl).then(function(response){
-						that.pg = response.data.value;
-					})           
-					.catch(function(error){
-						console.log(error);
-					})
+		getListData: function (lname) {
+			var endPointUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('" + lname + "')/items";
+			var headers = {
+				"accept": "application/json;odata=verbose"
+			};
+
+			var that = this;
+			axios.get(endPointUrl).then(function (response) {
+				that.pg = response.data.value;
+			})
+				.catch(function (error) {
+					console.log(error);
+				})
 
 		},
-		editForm: function(id){
-		alert(this.url+id);
+		editForm: function (id) {
+			alert(this.url + id);
 			//window.location.replace(this.url+id);
 		},
-		cascade: function(e){
-			var newPg = this.pg.filter(function(item){
-				console.log(item.Title+"="+e.target.value);
+		cascade: function (e) {
+			var newPg = this.pg.filter(function (item) {
+				console.log(item.Title + "=" + e.target.value);
 				return item.Title == e.target.value;
-			});			
+			});
 		}
 	},
 	computed: {
-	    filterRegions: function(){
-	      var selectedRegion = this.regionDD;
-	      var filter = this.searchTbl;
-	
-	      if (selectedRegion) {
-	        return this.pg.filter(function (item) {
-	          if (filter) {
-	            //return item.Region === selectedRegion && item.Site.startsWith(filter);
-	            return item.Title === selectedRegion && item.Site.toLowerCase().indexOf(filter.toLowerCase())>= 0;
-	          }
-	
-	          return item.Title === selectedRegion;
-	        });
-	      } else {
-	        return this.pg;
-	      }
-	    }
+		filterRegions: function () {
+			var selectedRegion = this.regionDD;
+			var filter = this.searchTbl;
+
+			if (selectedRegion) {
+				return this.pg.filter(function (item) {
+					if (filter) {
+						//return item.Region === selectedRegion && item.Site.startsWith(filter);
+						return item.Title === selectedRegion && item.Site.toLowerCase().indexOf(filter.toLowerCase()) >= 0;
+					}
+
+					return item.Title === selectedRegion;
+				});
+			} else {
+				return this.pg;
+			}
+		}
 	}
 });
